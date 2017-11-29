bd34ec53cccda24f3a5611136c858fc661ed42de
ianbogda <ianbogda@gmail;com>
2017-11-29 19:52:04 +0100

    Fix

--- app.js
+++ app.js
@@ -3,20 +3,17 @@ module.exports = function(sails) {
   return {
 
     // Method called by gladys to install/init module
-    install: require('./lib/install'),
+    install: require('./lib/install.js'),
 
     // Method called by gladys to notify
-    notify: require('./lib/notify'),
+    notify: require('./lib/notify.js'),
 
     // Those methods can be called in a script
-    // gladys.module.picotts.say({text:"MyText", language:"fr-fr"})
-    say: require('./lib/say')
+    // gladys.module.picotts.say({text:"MyText", language:"fr-FR"})
+    say: require('./lib/say.js'),
 
-    // gladys.module.picotts.say({text:"MyText", language:"fr-fr"})
-    repeat: require('./lib/repeat')
-
-    // gladys.module.picotts.say({text:"MyText", language:"fr-fr"})
-    shutup: require('./lib/shutup')
+    // gladys.module.picotts.say({text:"MyText", language:"fr-FR"})
+    repeat: require('./lib/say.js')
 
   };
 };
